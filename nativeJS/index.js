1d9a4219fe7b85f14663d83735cc039af842cf70
pawe999999 <77101060+pawe999999@users.noreply.github.com>
2021-08-19 12:15:03 +0200

    Update index.js

--- nativeJS/index.js
+++ nativeJS/index.js
@@ -1,9 +1,9 @@
-const addButton = document.getElementById("addButton");
-const parentDiv = document.getElementById("TODO-container");
+const addButton = document.getElementById('addButton');
+const parentDiv = document.getElementById('TODO-container');
 
 // class TODOapp {
 //   constructor() {
-//     this.input = document.getElementById("input-field");
+//     this.input = document.getElementById('input-field');
 //   }
 //
 //   edit(e) {
@@ -11,27 +11,27 @@ const parentDiv = document.getElementById("TODO-container");
 //   }
 //
 //   visiblity(e) {
-//     e.style.display = "none";
+//     e.style.display = 'none';
 //   }
 //   checkValid() {
 //     if (!this.input.checkValidity()) {
-//       alert("Wrong input");
-//       throw new Error("error");
+//       alert('Wrong input');
+//       throw new Error('error');
 //     }
 //   }
 //
 //   addEventListeners() {
-//     this.item.element.querySelector(".del").addEventListener("click", (e) => {
+//     this.item.element.querySelector('.del').addEventListener('click', (e) => {
 //       this.deleteItem(e.target.parentElement);
 //     });
-//     this.item.element.querySelector(".edit").addEventListener("click", (e) => {
-//       e.target.parentNode.querySelector(".modal").style.display = "block";
+//     this.item.element.querySelector('.edit').addEventListener('click', (e) => {
+//       e.target.parentNode.querySelector('.modal').style.display = 'block';
 //     });
-//     this.item.element.querySelector(".close").addEventListener("click", (e) => {
-//       e.target.parentNode.style.display = "none";
+//     this.item.element.querySelector('.close').addEventListener('click', (e) => {
+//       e.target.parentNode.style.display = 'none';
 //     });
-//     this.item.element.querySelector(".save").addEventListener("click", (e) => {
-//       e.target.parentNode.style.display = "none";
+//     this.item.element.querySelector('.save').addEventListener('click', (e) => {
+//       e.target.parentNode.style.display = 'none';
 //       this.edit(e.target.parentNode);
 //     });
 //   }
@@ -55,45 +55,90 @@ class TodoApp {
     //CRUD
     constructor() {
         this.todoItems = [];
-
-        this.input = document.getElementById("input-field");
-        this.deleteButton = document.querySelector(".del");
+        this.input = document.getElementById('input-field');
         //etc.
-
         this.modal = new ModalWindow();
-        this.initAllListeners();
+    }
+    initButtons() {
+        this.deleteButton = document.getElementsByClassName('del');
+        this.editButton = document.getElementsByClassName('edit');
+        this.closeButton = document.getElementsByClassName('close');
+        this.saveButton = document.getElementsByClassName('save');
     }
 
-    addTodoItem(title, startDate, expirationDate) {
-        const todoItem = new TodoItem(title);
+    addTodoItem(title) {
+        const todoItem = new TodoItem(this.input.value);
         this.todoItems.push(todoItem);
         this.render();
+        this.initButtons();
+        this.initAllListeners();
+        console.log(this.deleteButton);
     }
 
     render() {
-        //clear all list and render items from the list
+        parentDiv.innerHTML = '';
         const list = document.createElement('div');
-        this.todoItems.forEach((item) => list.appendChild(item.getTemplate()))
+        this.todoItems.forEach((item) => {
+            list.appendChild(item.getTemplate());
+            list.appendChild(this.modal.modal);
+        });
+        parentDiv.appendChild(list);
     }
 
-    edit(index, title, date) {
-        this.todoItems[index].edit(title, date);
-        this.render()
-    }
-
-    listenToTheInput() {
-        this.input.addEventListener('click', () => {
-            this.addTodoItem(this.input.value);
-        })
+    edit(index) {
+        this.todoItems[index].editItem();
+        this.render();
+        this.initButtons();
+        this.initAllListeners();
     }
 
     listenToTheModalOpen() {
-        this.modal.showModal();
+        this.modal.hideModal();
+    }
+    listenToTheSave() {
+        const saveNodes = Array.prototype.slice.call(this.saveButton);
+        for (let button of this.saveButton) {
+            button.addEventListener('click', () => {
+                this.edit(saveNodes.indexOf(button));
+            });
+        }
+    }
+    listenToTheDelete() {
+        const deleteNodes = Array.prototype.slice.call(this.deleteButton);
+        for (let button of this.deleteButton) {
+            button.addEventListener('click', () => {
+                console.log('delete');
+                this.delete(deleteNodes.indexOf(button));
+            });
+        }
+    }
+    listenToTheEdit() {
+        const editNodes = Array.prototype.slice.call(this.editButton);
+        for (let button of this.editButton) {
+            button.addEventListener('click', () => {
+                this.modal.showModal();
+            });
+        }
+    }
+    listenToTheClose() {
+        for (let button of this.closeButton) {
+            button.addEventListener('click', () => {
+                this.modal.hideModal();
+            });
+        }
+    }
+    delete(index) {
+        this.todoItems.splice(index, 1);
+        this.render();
+        this.initButtons();
+        this.initAllListeners();
     }
 
     initAllListeners() {
-        this.listenToTheModalOpen();
-        this.listenToTheInput();
+        this.listenToTheEdit();
+        this.listenToTheDelete();
+        this.listenToTheSave();
+        this.listenToTheClose();
     }
 }
 
@@ -106,40 +151,39 @@ class TodoItem {
     }
 
     getTemplate() {
-        const element = document.createElement("div");
-        element.classList.add("element");
-        this.modalWin = new ModalWindow();
-        element.innerHTML = `<p class="title">${this.title} </p>
-    <p class="start">${this.start} </p>
-    <p class="end"> ${this.end}  </p>
-    <p class="del"> DELETE</p> 
-    <p class="edit">EDIT</p> 
-    ${this.modalWin.modal.outerHTML}`;
+        const element = document.createElement('div');
+        element.classList.add('element');
+        element.innerHTML = `<p class='title'>${this.title} </p>
+        <p class='start'>${this.start} </p>
+        <p class='end'> ${this.end}  </p>
+        <p class='del'> DELETE</p> 
+        <p class='edit'>EDIT</p> 
+        `;
         return element;
     }
 
     editItem(title, date) {
-        this.title = title;
-        this.date = date;
+        console.error('Not Implemented');
     }
 }
 
 class ModalWindow {
     constructor() {
         this.crateModal();
+        this.hideModal();
     }
 
     crateModal() {
         this.modal = document.createElement('div');
-        this.modal.classList.add("modal");
+        this.modal.classList.add('modal');
         this.modal.innerHTML = `
-        <input class="startDate" type="date"> <br> 
-        <input class="titleInput" type="text"><br>
-        <input class="startTime" type="time" step="1"><br>
-        <input class="endDate" type="date"> <br> 
-        <input class="endTime" type="time" step="1"><br>
-        <p class="save">Save</p>
-        <p class="close">X</p> `;
+        <input class='startDate' type='date'> <br> 
+        <input class='titleInput' type='text'><br>
+        <input class='startTime' type='time' step='1'><br>
+        <input class='endDate' type='date'> <br> 
+        <input class='endTime' type='time' step='1'><br>
+        <p class='save'>Save</p>
+        <p class='close'>X</p> `;
     }
 
     showModal() {
@@ -151,15 +195,13 @@ class ModalWindow {
     }
 }
 
-const todoApp = new TODOapp();
-
-addButton.addEventListener("click", () => {
-    todoApp.addItem();
-});
-
-
 class App {
     constructor() {
         this.todoApp = new TodoApp();
     }
 }
+
+const app = new App();
+addButton.addEventListener('click', () => {
+    app.todoApp.addTodoItem();
+});
